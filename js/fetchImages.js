f9c780b614b891131febdec950ca05e3d7250126
sanjay kumar <sanjaykumar2763@gmail.com>
2018-05-12 22:11:45 +0530

    Event pages fetch images

--- js/fetchImages.js
+++ js/fetchImages.js
@@ -17,22 +17,51 @@ angular
 fetchController.$inject = ['$scope', '$firebaseArray'];
 
 function fetchController($scope, $firebaseArray) {
-	$firebaseArray(firebase.database().ref("bt/"))
-		.$loaded()
-		.then(function(response) {
-			$scope.btPath = response[0].url;
-			// console.log('response: ', response);
-		});
-	$firebaseArray(firebase.database().ref("wt/"))
-		.$loaded()
-		.then(function(response) {
-			$scope.wtPath = response[0].url;
-			// console.log('response: ', response);
-		});
-	$firebaseArray(firebase.database().ref("ct/"))
-		.$loaded()
-		.then(function(response) {
-			$scope.ctPath = response[0].url;
-			// console.log('response: ', response);
-		});
+	$scope.init = init;
+
+	function init(page) {
+		if(page === 'home') {
+			loadHomePageImages();
+		} else if(page === 'birthday') {
+			$firebaseArray(firebase.database().ref().child('bi/'))
+				.$loaded()
+				.then(function(response) {
+					$scope.images = response;
+					console.log('$scope.images: ', $scope.images);
+				});
+		} else if(page === 'wedding') {
+			$firebaseArray(firebase.database().ref().child('wi/'))
+				.$loaded()
+				.then(function(response) {
+					$scope.images = response;
+				});	
+		} else if(page === 'corporate') {
+			$firebaseArray(firebase.database().ref().child('ci/'))
+				.$loaded()
+				.then(function(response) {
+					$scope.images = response;
+				});
+		}
+	}
+
+	function loadHomePageImages() {
+		$firebaseArray(firebase.database().ref("bt/"))
+			.$loaded()
+			.then(function(response) {
+				$scope.btPath = response[0].url;
+				// console.log('response: ', response);
+			});
+		$firebaseArray(firebase.database().ref("wt/"))
+			.$loaded()
+			.then(function(response) {
+				$scope.wtPath = response[0].url;
+				// console.log('response: ', response);
+			});
+		$firebaseArray(firebase.database().ref("ct/"))
+			.$loaded()
+			.then(function(response) {
+				$scope.ctPath = response[0].url;
+				// console.log('response: ', response);
+			});
+	}
 }
